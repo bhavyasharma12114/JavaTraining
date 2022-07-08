e7e32433c2e3747147435fd42dff185b84932b14
Bhavya Sharma <60272427+bhavyasharma12114@users.noreply.github.com>
2022-07-08 14:32:53 +0530

    Q8AddElementEndLinkedList

---
+++ Java Training 2/Assignment2/Q8AddElementEndLinkedList.java
@@ -0,0 +1,21 @@
+package Assignment2;
+
+import java.util.LinkedList;
+public class Q8AddElementEndLinkedList {
+
+  public static void main(String[] args) {
+
+     LinkedList<String> list = new LinkedList<String>();
+
+     list.add("AA");
+     list.add("BB");
+     list.add("CC");
+     list.add("DD");
+ 
+     System.out.println("LinkedList Elements:"+list);
+
+     list.offerLast("NEW Element");
+
+     System.out.println("LinkedList After Addition:"+list);
+  }
+}	
