19c6b45a02a39bf6477c0431f4fe61ca9daf73de
Bhavya Sharma <60272427+bhavyasharma12114@users.noreply.github.com>
2022-07-08 14:21:28 +0530

    Q2 ArrayListRetrieveElement.java

---
+++ Java Training 2/Assignment2/Q2ArrayListRetrieveElement.java
@@ -0,0 +1,17 @@
+package Assignment2;
+	import java.util.*;
+	  public class Q2ArrayListRetrieveElement {
+	  public static void main(String[] args) {
+	  
+	  List<String> list_Strings = new ArrayList<String>();
+	  list_Strings.add("Red");
+	  list_Strings.add("Green");
+	  list_Strings.add("Orange");
+	  list_Strings.add("White");
+	  list_Strings.add("Black");
+	  
+	  System.out.println(list_Strings);
+	  System.out.println("First element: "+list_Strings.get(0));
+	  System.out.println("Third element: "+ list_Strings.get(2));
+	 }
+	}
