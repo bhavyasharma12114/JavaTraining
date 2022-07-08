9813fbdb5bdc1813303879d26acec3ef57f19aa5
Bhavya Sharma <60272427+bhavyasharma12114@users.noreply.github.com>
2022-07-08 14:23:00 +0530

    Q3UpdateElementArrayList

---
+++ Java Training 2/Assignment2/Q3UpdateElementArrayList.java
@@ -0,0 +1,20 @@
+package Assignment2;
+
+import java.util.*;
+public class Q3UpdateElementArrayList {
+public static void main(String[] args) {
+
+List<String> list_Strings = new ArrayList<String>();
+list_Strings.add("Red");
+list_Strings.add("Green");
+list_Strings.add("Orange");
+list_Strings.add("White");
+list_Strings.add("Black");
+
+System.out.println(list_Strings);
+list_Strings.set(2, "pink");
+list_Strings.set(2, "Yellow");
+//list_Strings.set(5, "Grey") ;
+System.out.println(list_Strings);
+}
+}
